bf50704368347346d35100ad0f2e7f01c2e62050
Sohail Shaikh <sohail@Sohails-MacBook-Air.local>
2026-04-11 23:07:39 +0100

    fix: correctly bundle renderer script for custom domain root

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Updated to root because you are using a custom domain (model-me.net)
+  base: '/', 
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',
-        renderer: 'renderer.html',
-      },
-    },
-  },
+        renderer: 'renderer.html'
+      }
+    }
+  }
 });
